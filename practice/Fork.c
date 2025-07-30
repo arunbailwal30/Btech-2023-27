99b809b7b09a6ba3617873f4e538b7c8dad03ab0
arun <arunbailwal30@gmail.com>
2025-07-30 21:11:20 +0530

    30-7-25

--- practice/Fork.c
+++ practice/Fork.c
@@ -4,7 +4,8 @@
 int main(){
     printf("Hello World\n");
     fork(); // Create a new process
-    fork(); // Create another new process
-    printf("Hello World\n");
+    fork(); // Create another new process // 
+    printf("Hello World\n"); // it is responsibility of the parent to clear the resources of the child process
+    sleep(1); // Sleep for 1 second to ensure all processes have time to print
     return 0;   
 }
