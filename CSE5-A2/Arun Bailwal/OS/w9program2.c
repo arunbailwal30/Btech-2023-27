2964e34963999c299d697862b81225f1c66714ca
arunbailwal30 <arunbailwal30@gmail.com>
2025-11-07 08:48:02 +0530

    07-11-25

---
+++ CSE5-A2/Arun Bailwal/OS/w9program2.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct file{
+    char name[100];
+    int index;
+    int *blocks;
+    int noBlock;
+}file;
+
+void linked(file *arr, int n){
+    for(int i=0;i<n;i++){
+        arr[i].blocks = (int*)malloc(sizeof(int) * arr[i].noBlock);
+        for(int j  = 0;j<arr[i].noBlock;j++){
+            if(j==0){
+                arr[i].blocks[j] = arr[i].index;
+                continue;
+            } 
+            arr[i].blocks[j] = arr[i].blocks[j-1]+1;
+        }
+    }
+
+    printf("Enter name of file: ");
+    char s[100];
+    scanf("%s",s);
+    printf("File Name  Start block  no. of blocks Blocks occupied\n");
+    for(int i =0;i<n;i++){
+        if(strcmp(arr[i].name,s)==0){
+            printf("%s \t\t %d \t %d \t\t",arr[i].name,arr[i].index ,arr[i].noBlock);
+            for(int j = 0; j<arr[i].noBlock;j++){
+                printf("%d, ", arr[i].blocks[j]);
+            }
+            break;
+        }
+    }
+    printf("\n");
+
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    file arr[n];
+    for(int i= 0;i<n;i++){
+        printf("Enter file%d name: ",i+1);
+        scanf("%s",(arr[i].name));
+        printf("Enter starting block number in file%d: ",i+1);
+        scanf("%d", &(arr[i].index));
+        printf("Enter number of blocks in file%d: ",i+1);
+        scanf("%d",&(arr[i].noBlock));
+    }
+
+    linked(arr, n);
+    return 0;
+}
